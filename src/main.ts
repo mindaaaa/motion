c849561ab5c195d9b6864c8490e5b8c20d29d61d
mindaaaa <devalc735@gmail.com>
2023-11-04 17:59:59 +0900

    feat: 모달 기능 구현

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const openButtons = document.querySelectorAll('.open');
+const modal = document.querySelector('.modal');
+const openModal = () => {
+  modal?.classList.remove('hidden');
+};
+openButtons.forEach((button) => {
+  button.addEventListener('click', openModal);
+});
+const closeButton = modal?.querySelector('.close');
+const closeModal = () => {
+  modal?.classList.add('hidden');
+};
+closeButton?.addEventListener('click', closeModal);
