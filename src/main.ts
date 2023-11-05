c60b51a46c00e726bf0a1d03385e0680ddec0f7d
mindaaaa <devalc735@gmail.com>
2023-11-06 02:58:38 +0900

    feat: modal flag를 통한 중복 모달 방지

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
 const openButtons = document.querySelectorAll('.open');
 const modals = document.querySelectorAll('.modal');
+let isModalOpen = false;
 
 openButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    const modalId = button.getAttribute('data-modal-id') as string;
-    console.log(modalId);
-    const modalToOpen = document.getElementById(modalId);
-    if (modalToOpen) {
-      modalToOpen.classList.remove('hidden');
+    if (!isModalOpen) {
+      const modalId = button.getAttribute('data-modal-id') as string;
+      console.log(modalId);
+      const modalToOpen = document.getElementById(modalId);
+      if (modalToOpen) {
+        modalToOpen.classList.remove('hidden');
+        isModalOpen = true;
+      }
     }
   });
 });
 
 modals.forEach((modal) => {
-  modal.addEventListener('click', () => modal.classList.add('hidden'));
+  modal.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    isModalOpen = false;
+  });
 });
