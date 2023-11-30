700b923621b79f13f7387fa9d68d7db3a05b4532
mindaaaa <devalc735@gmail.com>
2023-11-30 22:31:05 +0900

    feat: image, title 띄우기

--- src/main.ts
+++ src/main.ts
@@ -42,19 +42,47 @@ function handleCloseModal(event: Event) {
 }
 
 // add 버튼을 누르면
-// 우측 이미지/좌측 타이틀이 출력되고
+// input에 내용이 들어가서
+// img 태그가 출력되고, div 태그에 title이 출력되고
 // 모달이 꺼져요
-const modal = document.querySelector('#image-modal');
+modals.forEach((modal) => {
+  const modalTitle: HTMLElement | null =
+    document.querySelector('#modalContent');
+  const modalImage: HTMLImageElement | null =
+    document.querySelector('#modal-image');
+  const addButton = modal.querySelector('.add') as HTMLElement;
+  addButton.addEventListener('click', () => {
+    // 모달 타이틀 입력
+    const modalInputTitle = modal.querySelector(
+      '#image-title'
+    ) as HTMLInputElement;
+    const title = modalInputTitle.value;
 
-const addButton = modal?.querySelector('.add');
-addButton?.addEventListener('click', () => {
-  const imageUrlInput = document.getElementById(
-    'image-url'
-  ) as HTMLInputElement;
-  const imageUrl = imageUrlInput?.value;
+    if (modalTitle) {
+      modalTitle.textContent = `${title}`;
+    }
+    // 모달 이미지 출력
+    const modalInputImage = modal.querySelector(
+      '#image-url'
+    ) as HTMLInputElement;
+    const image = modalInputImage.value;
 
-  const modalImage = modal?.querySelector('#modal-image') as HTMLImageElement;
-  if (modalImage) {
-    modalImage.src = imageUrl;
-  }
+    if (modalImage) {
+      modalImage.src = image;
+    }
+    // 모달 삭제
+    handleCloseModal;
+  });
 });
+
+// addButton?.addEventListener('click', () => {
+//   const imageUrlInput = document.getElementById(
+//     'image-url'
+//   ) as HTMLInputElement;
+//   const imageUrl = imageUrlInput?.value;
+
+//   const modalImage = modal?.querySelector('#modal-image') as HTMLImageElement;
+//   if (modalImage) {
+//     modalImage.src = imageUrl;
+//   }
+// });
