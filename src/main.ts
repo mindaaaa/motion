bfd4b160b4343019eb3dedf024bb11569ed0cfc2
mindaaaa <devalc735@gmail.com>
2023-12-07 19:31:11 +0900

    feat: 모든 버튼이 동일한 비지니스 로직을 재사용하도록 함수 수정

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,25 @@ function handleOpenModal(event: Event) {
   }
 }
 
-const closeButton: NodeListOf<HTMLElement> =
+const closeButtons: NodeListOf<HTMLElement> =
   document.querySelectorAll('.close');
 
-closeButton.forEach((button) => {
-  button.addEventListener('click', handleCloseModal);
+closeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modalId = button.getAttribute('close-button-id');
+    if (modalId) {
+      closeModal(modalId);
+    }
+  });
 });
+
+function closeModal(modalId: string) {
+  const modalToClose = document.getElementById(modalId);
+  if (modalToClose) {
+    modalToClose.classList.add('hidden');
+    isModalOpen = false;
+  }
+}
 
 function handleCloseModal(event: Event) {
   const closeButton = event.currentTarget as HTMLElement;
@@ -43,21 +56,108 @@ function handleCloseModal(event: Event) {
 
 const contentElement = document.querySelector('#content') as HTMLElement;
 
+// modals.forEach((modal) => {
+//   const addButton = modal.querySelector('.add') as HTMLElement;
+//   const modalId = addButton.getAttribute('add-button-id');
+
+//   addButton.addEventListener('click', () => {
+//     const { title, image } = getInputValue(modal, modalId);
+
+//     if (title && image && modalId) {
+//       createSection(title, image, modalId);
+
+//       if (modalId) {
+//         closeModal(modalId);
+//       }
+//     }
+//   });
+// });
+
+// function getInputValue(
+//   modal: HTMLElement,
+//   modalId: string | null
+// ): { title: string; image: string } {
+//   let inputTitleId = '';
+//   let inputImageId = '';
+
+//   if (modalId === 'image-modal') {
+//     inputTitleId = '#image-title';
+//     inputImageId = '#image-url';
+//   } else if (modalId === 'video-modal') {
+//     inputTitleId = '#video-title';
+//     inputImageId = '#video-url';
+//   } else {
+//     inputTitleId = '.title';
+//     inputImageId = '.body';
+//   }
+
+//   const inputTitle = modal.querySelector(inputTitleId) as HTMLInputElement;
+//   const inputImage = modal.querySelector(inputImageId) as HTMLInputElement;
+
+//   return {
+//     title: inputTitle.value,
+//     image: inputImage.value,
+//   };
+// }
+
+// function createSection(title: string, image: string, modalId: string) {
+//   const newSection = document.createElement('section');
+//   newSection.classList.add('section-css');
+
+//   const newTitle = document.createElement('div');
+//   newTitle.textContent = title;
+//   const newItem: HTMLElement = document.createElement(modalId);
+
+//   if (modalId === 'image-modal' || modalId === 'video-modal') {
+//     newItem.setAttribute('src', image);
+//   } else {
+//     newItem.innerText = image;
+//   }
+
+//   newSection.appendChild(newItem);
+//   newSection.appendChild(newTitle);
+
+//   contentElement.appendChild(newSection);
+// }
+
 modals.forEach((modal) => {
   const addButton = modal.querySelector('.add') as HTMLElement;
-  addButton.addEventListener('click', () => {
-    const { title, image } = getInputValue(modal);
+  const modalType = addButton.getAttribute('modal-type');
 
-    if (title && image) {
-      const newSection = createSection(title, image);
-      contentElement.appendChild(newSection);
+  addButton.addEventListener('click', () => {
+    const { title, image } = getInputValue(modal, modalType);
+
+    if (title && image && modalType) {
+      createSection(title, image, modalType);
+
+      const modalId = addButton.getAttribute('add-button-id');
+      if (modalId) {
+        closeModal(modalId);
+      }
     }
   });
 });
 
-function getInputValue(modal: HTMLElement): { title: string; image: string } {
-  const inputTitle = modal.querySelector('#image-title') as HTMLInputElement;
-  const inputImage = modal.querySelector('#image-url') as HTMLInputElement;
+function getInputValue(
+  modal: HTMLElement,
+  modalType: string | null
+): { title: string; image: string } {
+  let inputTitleId = '';
+  let inputImageId = '';
+
+  if (modalType === 'img') {
+    inputTitleId = '#image-title';
+    inputImageId = '#image-url';
+  } else if (modalType === 'video') {
+    inputTitleId = '#video-title';
+    inputImageId = '#video-url';
+  } else {
+    inputTitleId = '.title';
+    inputImageId = '.body';
+  }
+
+  const inputTitle = modal.querySelector(inputTitleId) as HTMLInputElement;
+  const inputImage = modal.querySelector(inputImageId) as HTMLInputElement;
 
   return {
     title: inputTitle.value,
@@ -65,20 +165,33 @@ function getInputValue(modal: HTMLElement): { title: string; image: string } {
   };
 }
 
-function createSection(title: string, image: string): HTMLElement {
+function createSection(title: string, image: string, item: string) {
   const newSection = document.createElement('section');
   newSection.classList.add('section-css');
 
   const newTitle = document.createElement('div');
   newTitle.textContent = title;
-  const newImg = document.createElement('img');
-  newImg.src = image;
+  const newItem: HTMLElement = document.createElement(item);
 
-  newSection.appendChild(newImg);
+  if (item === 'img') {
+    newItem.setAttribute('src', image);
+  } else if (item === 'video') {
+    const sourceElement = document.createElement('source');
+    sourceElement.setAttribute('src', image);
+    newItem.appendChild(sourceElement);
+    newItem.setAttribute('width', '400');
+    newItem.setAttribute('height', '300');
+    newItem.setAttribute('controls', 'controls');
+  } else {
+    newItem.innerText = image;
+  }
+
+  newSection.appendChild(newItem);
   newSection.appendChild(newTitle);
 
-  return newSection;
+  contentElement.appendChild(newSection);
 }
 
-// add 버튼을 누르면
-// 모달이 꺼지요
+// todo ◼로 구현
+// 드래그앤드롭
+// css
